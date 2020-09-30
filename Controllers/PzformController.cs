b9e5602bc2a5334e8d56798d4131884cf043497e
Brad Wilkes <aldelta19@gmail.com>
2020-09-29 21:10:48 -0500

    First real commit

---
+++ Controllers/PzformController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Pegazeus.Controllers
+{
+    public class PzformController : Controller
+    {
+        // 
+        // GET: /Pegazeus/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /Pegazeus/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pegazeus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
